9d45634696f9f37c6432e91db77b5fe3623b0db9
Archlight001 <archlight001@gmail.com>
2020-11-12 14:39:39 +0100

    Create Project with ShapeSelector Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ShapeSelector from "./ShapeSelector";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="main__title">LIVE SHAPES</h1>
+      
+      <ShapeSelector />
     </div>
   );
 }
---
+++ src/ShapeSelector.jsx
@@ -0,0 +1,60 @@
+import Triangle from "./shapes/triangle.png";
+import Rectangle from "./shapes/rectangle.png";
+import Square from "./shapes/square.png";
+import Pentagon from "./shapes/pentagon.png";
+import Hexagon from "./shapes/hexagon.png";
+import Parallelogram from "./shapes/parallelogram.png";
+
+function ShapeSelector() {
+  return (
+    <div className="main__shape__selector__container">
+      <h3>Select a Shape </h3>
+
+      <div className="shape__list">
+        <div className="shape__selector">
+          <img
+            src={Triangle}
+            id="triangle"
+            width="20"
+            height="20"
+            alt="triangle"
+          />
+          <p>Triangle</p>
+        </div>
+
+        <div className="shape__selector">
+          <img src={Rectangle} width="40" alt="rectangle" />
+          <p>Rectangle</p>
+        </div>
+
+        <div className="shape__selector">
+          <img src={Square} width="40" id="square" height="20" alt="square" />
+          <p id="square__p">Square</p>
+        </div>
+
+        <div className="shape__selector">
+          <img src={Parallelogram} id="parallelogram" width="40" height="35" alt="parallelogram" />
+          <p>Parallelogram</p>
+        </div>
+
+        <div className="shape__selector">
+          <img src={Pentagon} id="pentagon" width="40" height="35" alt="pentagon" />
+          <p>Pentagon</p>
+        </div>
+
+        <div className="shape__selector">
+          <img src={Hexagon} width="40" id="hexagon" height="35" alt="hexagon" />
+          <p>Hexagon</p>
+        </div>
+      </div>
+
+      {/* 
+            
+            
+            
+             */}
+    </div>
+  );
+}
+
+export default ShapeSelector;
